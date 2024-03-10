9f9a2870c967a0c068cce8932007221ae6fb39bd
hideo333666 <muramura031409@gmail.com>
2024-03-10 18:13:17 +0900

    [fix]DOM要素追加処理完成

--- main.js
+++ main.js
@@ -1,8 +1,30 @@
 import './style.css'
 
 const onClickAdd = () => {
-    alert();
+    // テキストボックスの値を取得し、初期化する
+    const inputText = document.getElementById("add-text").value;
+    document.getElementById("add-text").value = "";
+
+    // li生成
+    const li = document.createElement("li");
+
+    // div生成
+    const div = document.createElement("div");
+    div.className = "list-row";
+
+    // p生成
+    const p = document.createElement("p");
+    p.className = "todo-item";
+    p.innerText = inputText;
+
+    div.appendChild(p);
+    li.appendChild(div);
+
+    document.getElementById("incomplete-list").appendChild(li);
+
+    console.log(ul);
 }
+
 
 document.getElementById("add-button").addEventListener("click", onClickAdd);
 
